d170d37a7724bca90d9dc7a46b0fe046ac5936a6
swapnil lothe <44019530+swapnillothe@users.noreply.github.com>
2019-01-17 14:53:00 +0530

    Intiate flower catalogue project.
    needs to add referrence for guestBook.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
+const fs = require('fs');
+
+const getFilePath = function (url) {
+  if (url == '/') return './index.html';
+  // if(url=='/main.css')return '/main.css';
+  return './src/public' + url;
+}
+
 const app = (req, res) => {
-  res.statusCode = 404;
-  res.end();
+  let filePath = getFilePath(req.url);
+  fs.readFile(filePath, (err, content) => {
+    try {
+      res.write(content);
+      res.statusCode = 200;
+      res.end();
+    } catch (err) {
+      res.end();
+    }
+  })
 };
 
 // Export a function that can act as a handler
