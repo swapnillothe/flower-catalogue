64ec3418ea57703f6de6ef6987ea907c3a8085d7
swapnil lothe <44019530+swapnillothe@users.noreply.github.com>
2019-01-18 14:38:32 +0530

    Implement shighra frameWork.

--- src/app.js
+++ src/app.js
@@ -5,11 +5,33 @@ const getFilePath = function (url) {
   return './src/public' + url;
 }
 
+const send = (res, content, statusCode = 200) => {
+  res.write(content);
+  res.statusCode = statusCode;
+  res.end();
+}
+
+const showFile = (req, res, filePath) => {
+  fs.readFile(filePath, (err, content) => {
+    try {
+      send(res, content);
+    } catch (err) {
+      send(res, '404 - The page cannot be found', 404);
+    }
+    return;
+  });
+}
+
+const handleRequest = function (req, res) {
+  const filePath = getFilePath(req.url);
+  showFile(req, res, filePath);
+}
+
 const parseArgs = function (args) {
   const parsedArgs = {};
   const argsToParse = args.split('&').map(x => x.split('='));
   argsToParse.map(arg => {
-    parsedArgs[arg[0]] = arg[1].replace(/\+/g,' ');
+    parsedArgs[arg[0]] = arg[1].replace(/\+/g, ' ');
   });
   const date = new Date().toLocaleString();
   parsedArgs.date = date;
@@ -21,7 +43,6 @@ const writeComments = function (req, res) {
   req.on('data', (chunk) => {
     commentsData += chunk;
   });
-
   req.on('end', () => {
     fs.readFile('./src/public/commentsData.json', 'utf8', (err, comments) => {
       let commentsJsonData = JSON.parse(comments);
@@ -33,25 +54,34 @@ const writeComments = function (req, res) {
   });
 }
 
-const app = (req, res) => {
-  if (req.method == 'GET') {
-    const filePath = getFilePath(req.url);
-    fs.readFile(filePath, (err, content) => {
-      try {
-        res.write(content);
-        res.statusCode = 200;
-        res.end();
-      } catch (err) {
-        res.statusCode = 404;
-        res.write('404 - The page cannot be found');
-        res.end();
-      }
-    });
-    return;
+const isSameUrl = (route1, route2) => {
+  return route1.url == route2.url &&
+    route1.method == route2.method;
+}
+
+class App {
+  constructor() {
+    this.routes = [];
   }
-  writeComments(req, res);
-};
 
-// Export a function that can act as a handler
+  get(url, handler) {
+    const route = { url, handler, method: 'GET' };
+    this.routes.push(route);
+  }
 
-module.exports = app;
+  handleRequest(req, res) {
+    if (this.routes.some(isSameUrl.bind(null, req))) {
+      this.routes[0].handler(req, res);
+    }
+  }
+}
+
+const app = new App();
+app.get('/', handleRequest);
+app.get('/main.css', handleRequest);
+app.get('/waterJar.js', handleRequest);
+app.get('/photos/freshorigins.jpg', handleRequest);
+app.get('/photos/animated-flower-image-0021.gif', handleRequest);
+app.get('/guestBook.html', handleRequest);
+
+module.exports = app.handleRequest.bind(app);
