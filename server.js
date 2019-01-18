9ebb4944eb8670fed2a9a71b0d6aae9c59c3adc5
swapnil lothe <44019530+swapnillothe@users.noreply.github.com>
2019-01-18 12:28:01 +0530

    ready to collect comments in single file.

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const http = require('http');
 const app = require('./src/app.js');
 
-// Do not change the port value
-// If you want to use a different port
-// set it as an environment variable.
 const PORT = process.env.PORT || 8080;
 
 let server = http.createServer(app);
--- src/app.js
+++ src/app.js
@@ -5,19 +5,51 @@ const getFilePath = function (url) {
   return './src/public' + url;
 }
 
+const parseArgs = function (args) {
+  const parsedArgs = {};
+  const argsToParse = args.split('&').map(x => x.split('='));
+  argsToParse.map(arg => {
+    parsedArgs[arg[0]] = arg[1].replace(/\+/g,' ');
+  });
+  const date = new Date().toLocaleString();
+  parsedArgs.date = date;
+  return parsedArgs;
+}
+
+const writeComments = function (req, res) {
+  let commentsData = '';
+  req.on('data', (chunk) => {
+    commentsData += chunk;
+  });
+
+  req.on('end', () => {
+    fs.readFile('./src/public/commentsData.json', 'utf8', (err, comments) => {
+      let commentsJsonData = JSON.parse(comments);
+      commentsJsonData.push(parseArgs(commentsData));
+      comments = JSON.stringify(commentsJsonData);
+      fs.writeFile('./src/public/commentsData.json', comments, () => { });
+    });
+    res.end();
+  });
+}
+
 const app = (req, res) => {
-  const filePath = getFilePath(req.url);
-  fs.readFile(filePath, (err, content) => {
-    try {
-      res.write(content);
-      res.statusCode = 200;
-      res.end();
-    } catch (err) {
-      res.statusCode = 404;
-      res.write('404 - The page cannot be found');
-      res.end();
-    }
-  })
+  if (req.method == 'GET') {
+    const filePath = getFilePath(req.url);
+    fs.readFile(filePath, (err, content) => {
+      try {
+        res.write(content);
+        res.statusCode = 200;
+        res.end();
+      } catch (err) {
+        res.statusCode = 404;
+        res.write('404 - The page cannot be found');
+        res.end();
+      }
+    });
+    return;
+  }
+  writeComments(req, res);
 };
 
 // Export a function that can act as a handler
